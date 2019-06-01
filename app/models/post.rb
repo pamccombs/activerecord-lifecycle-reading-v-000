fd12176468c3fba990a9788ac958bc74a393d36f
Patrick McCombs <pa.mccombs1@gmail.com>
2019-06-01 23:57:46 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,14 @@ class Post < ActiveRecord::Base
 
   belongs_to :author
   validate :is_title_case 
+  
+  before_validation :make_title_case
+  
+  #before_save :email_author_about_post
+  
+  def edit
+    
+  end
 
   private
 
@@ -14,4 +22,6 @@ class Post < ActiveRecord::Base
   def make_title_case
     self.title = self.title.titlecase
   end
+  
+  
 end
